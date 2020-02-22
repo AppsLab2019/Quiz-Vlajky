e9972f86c61beadad0fed635acd1c5a40db3b92e
luboslavistenik <luboslav.istenik@gmail.com>
2020-02-22 22:11:01 +0100

    -

--- Quiz_Vlajky/Quiz_Vlajky/MainPage.xaml.cs
+++ Quiz_Vlajky/Quiz_Vlajky/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace Quiz_Vlajky
 {
@@ -17,42 +18,42 @@ namespace Quiz_Vlajky
         {
             "Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina", "Croatia", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary",
             "Iceland", "Ireland", "Italy", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Norway", "Poland", "Portugal",
-            "Romania", "Russia", "San Marino", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Ukraine", "United Kingdom"
+            "Romania", "Russia", "San_Marino", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Ukraine", "United Kingdom"
         };
         private readonly ImageButton[] Buttons;
+        private int round = 0;
 
-    public MainPage()
+        public MainPage()
         {
             InitializeComponent();
-            GetRandomCountry();
             Buttons = new[] { btn1, btn2, btn3, btn4 };
             foreach(var btn in Buttons)
             {
                 btn.Source = "";
             }
-        }
-
-        private void ImageButton_Clicked_1(object sender, EventArgs e)
-        {
-            
-           
+            GetRandomCountry();
         }
         private void GetRandomCountry()
         {
-            var rnd = new Random();
-            int countryIndex = rnd.Next(0, euCountries.Count - 1);
-            Country.Text = euCountries[countryIndex];
+                var rnd = new Random();
+                int countryIndex = rnd.Next(0, euCountries.Count - 1);
+                Country.Text = euCountries[countryIndex];
+                int flagBtn = rnd.Next(0, 3);
 
-            int flagBtn = rnd.Next(0, 3);
-            Buttons[flagBtn].Source = $"File: Slovakia.png";
+
         }
+        private void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+            
+        } 
         private void ImageButton_Clicked_2(object sender, EventArgs e)
         {
-            
+
         }
+
         private void ImageButton_Clicked_3(object sender, EventArgs e)
         {
-            
+
         }
         private void ImageButton_Clicked_4(object sender, EventArgs e)
         {
